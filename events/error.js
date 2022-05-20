a65f179a7b9ab474c14ce5c3ffa91717165ccfa6
Harrison <hhovland@isoftdata.com>
2022-05-20 13:10:18 -0500

    change

--- events/error.js
+++
@@ -1,5 +0,0 @@
-const logger = require("../modules/logger.js")
-//eslint-disable-next-line
-module.exports = async(client, error) => {
-	logger.log(`An error event was sent by Discord.js: \n${JSON.stringify(error)}`, "error")
-}
